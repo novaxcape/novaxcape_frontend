8cd4e07f675925601f4c1abbe6d8dad455feace4
novaxcape <gloriaekurume05@gmail.com>
2026-06-02 01:58:00 -0700

    i

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Login from './Pages/Login';
 import ForgotPassword from './Pages/ForgotPassword';
 import VerifyOtp from './Pages/VerifyOtp';
 import ResetPassword from './Pages/ResetPassword';
+import ProductDetail from './Pages/ProductDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignUp />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/verify-otp" element={<VerifyOtp />} />
